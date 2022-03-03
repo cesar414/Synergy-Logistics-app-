17d7b9622b4812445c3079ad2f2e0a3b7cb2c570
cesar414 <80677083+cesar414@users.noreply.github.com>
2022-03-02 18:42:05 -0600

    Update ANALISIS_02_ Marin Zurita_Cesar Alexander.py

--- ANALISIS_02_ Marin Zurita_Cesar Alexander.py
+++ ANALISIS_02_ Marin Zurita_Cesar Alexander.py
@@ -24,19 +24,18 @@ MenuPrincipal="""
 #funcion para calcular el flujo de trasporte 
 def flujo(fila):
     """
-    multiplico la cantidad trasportada por la frecuencia de flujo 
-    para obtener el flujo de trasporte que pasa por la ruta
-    para mejorar la tabla se divide entre 10 mil el flujo 
-
+    multiplico la Valor total de importaciones y exportaciones
+    por la frecuencia de flujo para obtener el flujo de trasporte 
+    que pasa por la ruta para mejorar la tabla se divide entre 10 mil el flujo 
     """  
-    resultado=(fila['cantidad trasportada']*fila['frecuencia de uso'])/10000
+    resultado=(fila['Valor total de importaciones y exportaciones']*fila['frecuencia de uso'])/10000
     return resultado
 #funcion para calcular el porcentaje que aporta cada pais
 def porcentaje(fila,total=países['total_value'].sum()):
     """
     para lograrlo se divide 
-    la cantidad trasportada / el total de cantidad trasportada hasta el momento 
-    por 100
+    Valor total de importaciones y exportaciones / el total de Valor total de importaciones y exportaciones
+    hasta el momento por 100
     """
     resultado=(fila['total_value']/total)*100
     return resultado
@@ -45,13 +44,14 @@ def menu1():
     #  Rutas mas importantes de importación y exportación 
     """
     creo el dataframe que contendra las rutas donde contenga cada fila 
-    la cantidad de trasporte de cada ruta se obtiene al usar la funcion sum()
-    la frecuencia de uso que tiene cada ruta se obtiene al usar la funcion count()
+    la Valor total de importaciones y exportaciones de cada ruta se obtiene al usar la funcion sum()
+    la frecuencia de uso que tiene cada ruta se obtiene al usar la funcion count() 
     también  renombro las columnas para mejorar el entendimiento de estas
     """
     rutas=tabla_date.groupby(["origin","destination",'transport_mode']).agg({
-        "total_value":'sum','company_name':'count'}).rename(columns={
-        'total_value':'cantidad trasportada','company_name':'frecuencia de uso'})
+        "total_value":'sum','company_name':'count'})
+    rutas=rutas.rename(columns={'total_value':'Valor total de importaciones y exportaciones',
+        'company_name':'frecuencia de uso'})
     #introduzco la columna flujo por 10 mil, mediante la función  flujo
     rutas['flujo por 10 mil']=rutas.apply(flujo,axis=1)
     #creo el dataframe que tendrá  las 10 primeras rutas 
@@ -64,39 +64,40 @@ def menu2():
     #Medio de transporte utilizado
     """
     Creo el dataframe que contendra los medios de trasporte por año donde contenga cada fila
-    la cantidad de trasporte que pasa por ese medio 
+    la Valor total de importaciones y exportaciones que pasa por ese medio 
     para poder graficar se sube year y transport_mode para conformar una columna 
     """
     medio=tabla_date.groupby(['year','transport_mode']).agg({"total_value":'sum'}).rename(
-        columns={'total_value':'Cantidad de trasporte'}).reset_index(level=['year','transport_mode'])
+        columns={'total_value':'Valor total de importaciones y exportaciones'}).reset_index(
+        level=['year','transport_mode'])
     """
-    creo un datafreme que estara ordenado por la cantidad de trasporte del mayor al menor
-    y renombro year y transport_mode
+    creo un datafreme que estara ordenado por la Valor total de importaciones y exportaciones
+    del mayor al menor y renombro year y transport_mode
     """
-    medio_grafica=medio.sort_values('Cantidad de trasporte',ascending=0).rename(
+    medio_grafica=medio.sort_values('Valor total de importaciones y exportaciones',ascending=0).rename(
         columns={'transport_mode':'Modo de trasporte','year':'Año'})
     #a continuacion imprimo una grafica de tiempo de los modos de trasportes 
     sns.set_theme(style='darkgrid')
-    sns.relplot(x="Año", y="Cantidad de trasporte", hue="Modo de trasporte", 
+    sns.relplot(x="Año", y="Valor total de importaciones y exportaciones", hue="Modo de trasporte", 
         kind="line",data=medio_grafica)
 
 def menu3():
     #Valor total de importaciones y exportaciones
     """
-    haciendo uso de las variables fijas se crea un dataframe países por la cantidad de trasporte
-    con una columna donde se generara el porcentaje acumulado usando la funcion porcentaje y cumsum() 
+    haciendo uso de las variables fijas se crea un dataframe países por la Valor total de importaciones y 
+    exportaciones con una columna donde se generara el porcentaje acumulado usando la funcion porcentaje y cumsum() 
     """
     países_orden['porcentaje_acumulado']=países_orden.apply(porcentaje,axis=1).cumsum()
     """
     ordeno el dataframe de mayor a menor segun su porcentaje acomulado 
-    y renombro total_value por cantidad trasportada
+    y renombro total_value por Valor total de importaciones y exportaciones
     """
     acumulado=países_orden.sort_values('porcentaje_acumulado').rename(columns={
-        'total_value':'Cantidad trasportada'})
+        'total_value':'Valor total de importaciones y exportaciones'})
     #me quedo solo con los datos que generan el 80% acomulado 
     paises_grafica=acumulado[acumulado.porcentaje_acumulado<80]
     #mediante una grafica de pastel imprimo los datos resultantes 
-    paises_grafica.plot(kind='pie',y='Cantidad trasportada', subplots=True, 
+    paises_grafica.plot(kind='pie',y='Valor total de importaciones y exportaciones', subplots=True, 
         shadow = True,startangle=30,
     figsize=(15,12), autopct='%1.1f%%')
 #menu principal creado mediante while 
@@ -113,6 +114,4 @@ while not UsuarioMenu:
         exit() 
     else: 
         print("opcion no valida")
-
-
 
